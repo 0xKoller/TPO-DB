46cec80b024b0bd78e425440c19f40051d29b51e
VeintiTreinta <matisantoro10@gmail.com>
2022-06-29 10:33:48 -0300

    cambios de fk

--- PrimeraParte_ParteA.sql
+++ PrimeraParte_ParteA.sql
@@ -1,16 +1,3 @@
-create table Albumes (
-	codAlbum int,
-	titulo varchar(200),
-	fecha date,
-	codArtista int,
-	codGenero int,
-	constraint pk_Albumes primary key (codAlbum)
-)
-
-insert into Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (1,'Future Nostalgia', '27/03/2020', 1, 1)
-insert into Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (2,'JODRI (Deluxe)', '11/06/2021', 2, 1)
-insert into Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (3,'TRAP DE VERDAD', '11/11/2021', 3, 2)
-
 create table Artista(
 	codArtista int,
 	nombre varchar(100),
@@ -30,12 +17,29 @@ create table Generos(
 insert into Generos (codGenero, descripcion) VALUES (1, 'POP')
 insert into Generos (codGenero, descripcion) VALUES (2, 'TRAP')
 
+create table Albumes (
+	codAlbum int,
+	titulo varchar(200),
+	fecha date,
+	codArtista int,
+	codGenero int,
+	constraint pk_Albumes primary key (codAlbum),
+	constraint fk_Artista foreign key (codArtista) references Artista (codArtista),
+	constraint fk_Genero foreign key (codGenero) references Generos (codGenero)
+)
+
+insert into Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (1,'Future Nostalgia', '27/03/2020', 1, 1)
+insert into Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (2,'JODRI (Deluxe)', '11/06/2021', 2, 1)
+insert into Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (3,'TRAP DE VERDAD', '11/11/2021', 3, 2)
+
+
 create table Temas(
 	codTema int,
 	titulo varchar(100),
 	duracion float,
 	codAlbum int,
-	constraint pk_Temas primary key (codTema)
+	constraint pk_Temas primary key (codTema),
+	constraint fk_Album foreign key (codAlbum) references Albumes (codAlbum)
 )
 
 insert into Temas (codTema, titulo,duracion,codAlbum) VALUES (1, 'Future Nostalgia', 3.04, 1)
@@ -84,7 +88,9 @@ create table Facturas (
 	iva float,
 	IIBB float,
 	final float,
-	constraint pk_Facturas primary key (nroFactura)
+	constraint pk_Facturas primary key (nroFactura),
+	constraint fk_Cliente foreign key (codCliente) references Cliente (codCliente),
+	constraint fk_Pedido foreign key (codPedido) references Pedidos (nroPedido)
 )
 
 insert into Facturas (nroFactura, fecha, codCliente,codPedido,bruto,iva,IIBB,final) VALUES (95, '28/06/2022', 01, 11,250.00,10.0,0.0,275.0)
@@ -108,8 +114,8 @@ create table ItemsPedido (
 	codProducto int,
 	codPedido int,
 	cantidad int
-	constraint fk_ItemsPedido_Productos foreign key (codProducto) references Productos,
-	constraint fk_ItemsPedido_Pedido foreign key (codPedido) references Pedidos
+	constraint fk_ItemsPedido_Productos foreign key (codProducto) references Productos (codProducto),
+	constraint fk_ItemsPedido_Pedido foreign key (codPedido) references Pedidos (nroPedido)
 )
 
 insert into ItemsPedido (codProducto, codPedido, cantidad) VALUES (01, 11, 1)
@@ -122,8 +128,8 @@ create table ItemsFactura(
 	cantidad int,
 	precio float,
 	descuento float,
-	constraint fk_ItemsFactura_Productos foreign key (codProducto) references Productos,
-	constraint fk_ItemsFactura_Factura foreign key (nroFactura) references Facturas
+	constraint fk_ItemsFactura_Productos foreign key (codProducto) references Productos (codProducto),
+	constraint fk_ItemsFactura_Factura foreign key (nroFactura) references Facturas (nroFactura)
 )
 
 insert into ItemsFactura (codProducto, nroFactura, cantidad, precio, descuento) VALUES (01, 95, 1,275,0)
@@ -134,8 +140,16 @@ create table Composiciones(
 	codProducto int,
 	codProductoComponente int,
 	cantidad int
+
+	constraint pk_ProductoComponente primary key (codProductoComponente),
+	constraint fk_Producto foreign key (codProducto) references Productos (codProducto)
 )
-/*Da fuk is composiones*/
+
+insert into Composiciones VALUES (01,01,100)
+insert into Composiciones VALUES (25,02,25)
+insert into Composiciones VALUES (99,03,400)
+insert into Composiciones VALUES (25,04,50)
+
 go
 
 
