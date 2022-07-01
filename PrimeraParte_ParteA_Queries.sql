a96b292ffdcc9f6452c203a0533da637834576e2
Jose Luis Koller <joseluiskoller98@gmail.com>
2022-07-01 10:56:41 -0300

    Dejandolo mas lindo y organizado

--- PrimeraParte_ParteA_Queries.sql
+++ PrimeraParte_ParteA_Queries.sql
@@ -3,3 +3,12 @@ SELECT COUNT (*) as CantTemas FROM Temas T INNER JOIN Albumes A ON A.codAlbum = 
 
 --Ejer 2
 SELECT COUNT (*) as CantArtistas FROM Artista A INNER JOIN Albumes AL ON A.codArtista = AL. CodArtista INNER J0IN Generos G ON G. codGenero-AL. codGenero WHERE G.descripcion = 'CLASICO'
+
+--Ejer 3
+
+
+
+
+
+
+
--- PrimeraParte_ParteA_Tablas.sql
+++ PrimeraParte_ParteA_Tablas.sql
@@ -1,15 +1,11 @@
 create database PrimeraParte_ParteA
 go
-
+set dateformat dmy
 create table PrimeraParte_ParteA.dbo.Artista(
 	codArtista int,
 	nombre varchar(100),
 	constraint pk_Artista primary key (codArtista)
 )
-
-insert into PrimeraParte_ParteA.dbo.Artista (codArtista, nombre) VALUES (1, 'Dua Lipa')
-insert into PrimeraParte_ParteA.dbo.Artista (codArtista, nombre) VALUES (2, 'Maroon 5')
-insert into PrimeraParte_ParteA.dbo.Artista (codArtista, nombre) VALUES (3, 'YSY A')
 go
 
 create table PrimeraParte_ParteA.dbo.Generos(
@@ -17,9 +13,6 @@ create table PrimeraParte_ParteA.dbo.Generos(
 	descripcion varchar(200),
 	constraint pk_Generos primary key (codGenero)
 )
-
-insert into PrimeraParte_ParteA.dbo.Generos (codGenero, descripcion) VALUES (1, 'POP')
-insert into PrimeraParte_ParteA.dbo.Generos (codGenero, descripcion) VALUES (2, 'TRAP')
 go
 
 create table PrimeraParte_ParteA.dbo.Albumes (
@@ -32,11 +25,6 @@ create table PrimeraParte_ParteA.dbo.Albumes (
 	constraint fk_Artista foreign key (codArtista) references PrimeraParte_ParteA.dbo.Artista (codArtista),
 	constraint fk_Genero foreign key (codGenero) references PrimeraParte_ParteA.dbo.Generos (codGenero)
 )
-
-set dateformat dmy
-insert into PrimeraParte_ParteA.dbo.Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (1,'Future Nostalgia', '27-03-2020', 1, 1)
-insert into PrimeraParte_ParteA.dbo.Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (2,'JODRI (Deluxe)', '11-06-2021', 2, 1)
-insert into PrimeraParte_ParteA.dbo.Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (3,'TRAP DE VERDAD', '11-11-2021', 3, 2)
 go
 
 create table PrimeraParte_ParteA.dbo.Temas(
@@ -47,18 +35,6 @@ create table PrimeraParte_ParteA.dbo.Temas(
 	constraint pk_Temas primary key (codTema),
 	constraint fk_Album foreign key (codAlbum) references PrimeraParte_ParteA.dbo.Albumes (codAlbum)
 )
-
-insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (1, 'Future Nostalgia', 3.04, 1)
-insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (2, 'Dont Start Now', 3.03, 1)
-insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (3, 'Levitating', 3.23, 1)
-
-insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (4, 'Beautiful Mistakes', 3.47, 2)
-insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (5, 'Lost', 2.53, 2)
-insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (6, 'Echo', 2.59, 2)
-
-insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (7, 'TRAP DE VERDAD', 2.12, 3)
-insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (8, 'ORO Y PLATINO', 2.47, 3)
-insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (9, 'INSPIRACION DIVINA', 3.11, 3)
 go
 
 create table PrimeraParte_ParteA.dbo.Cliente (
@@ -67,10 +43,6 @@ create table PrimeraParte_ParteA.dbo.Cliente (
 	saldo float,
 	constraint pk_Cliente primary key (codCliente)
 )
-
-insert into PrimeraParte_ParteA.dbo.Cliente (codCliente, nombre, saldo) VALUES (1, 'Jose Luis', 87.40)
-insert into PrimeraParte_ParteA.dbo.Cliente (codCliente, nombre, saldo) VALUES (2, 'Constantino', 7.30)
-insert into PrimeraParte_ParteA.dbo.Cliente (codCliente, nombre, saldo) VALUES (3, 'Matias', 999.99)
 go
 
 create table PrimeraParte_ParteA.dbo.Pedidos (
@@ -81,11 +53,6 @@ create table PrimeraParte_ParteA.dbo.Pedidos (
 	lugarEntrega varchar(200),
 	constraint pk_Pedidos primary key (nroPedido)
 )
-
-set dateformat dmy
-insert into PrimeraParte_ParteA.dbo.Pedidos (nroPedido, fecha, codCliente,fechaEntrega,lugarEntrega) VALUES (1, '28-06-2022', 1, '01-07-2022', 'Bernardo de Irigoyen 924')
-insert into PrimeraParte_ParteA.dbo.Pedidos (nroPedido, fecha, codCliente,fechaEntrega,lugarEntrega) VALUES (2, '30-06-2022', 3, '23-07-2022', 'Lima 755')
-insert into PrimeraParte_ParteA.dbo.Pedidos (nroPedido, fecha, codCliente,fechaEntrega,lugarEntrega) VALUES (3, '02-06-2022', 2, '10-06-2022', 'Av. Independencia 985')
 go
 
 create table PrimeraParte_ParteA.dbo.Facturas (
@@ -101,11 +68,6 @@ create table PrimeraParte_ParteA.dbo.Facturas (
 	constraint fk_Cliente foreign key (codCliente) references PrimeraParte_ParteA.dbo.Cliente (codCliente),
 	constraint fk_Pedido foreign key (codPedido) references PrimeraParte_ParteA.dbo.Pedidos (nroPedido)
 )
-
-set dateformat dmy
-insert into PrimeraParte_ParteA.dbo.Facturas (nroFactura, fecha, codCliente,codPedido,bruto,iva,IIBB,final) VALUES (1, '28-06-2022', 1, 1,250.00,10.0,0.0,275.0)
-insert into PrimeraParte_ParteA.dbo.Facturas (nroFactura, fecha, codCliente,codPedido,bruto,iva,IIBB,final) VALUES (2, '30-06-2022', 3, 2,2000.00,10.0,0.0,2200.0)
-insert into PrimeraParte_ParteA.dbo.Facturas (nroFactura, fecha, codCliente,codPedido,bruto,iva,IIBB,final) VALUES (3, '02-06-2022', 2, 1,180.00,10.0,0.0,198.0)
 go
 
 create table PrimeraParte_ParteA.dbo.Productos (
@@ -116,10 +78,6 @@ create table PrimeraParte_ParteA.dbo.Productos (
 	precio float,
 	constraint pk_Productos primary key (codProducto)
 )
-
-insert into PrimeraParte_ParteA.dbo.Productos (codProducto, descripcion, stock,puntoReposicion,precio) VALUES (1, 'Walkman', 15, 5,250.00)
-insert into PrimeraParte_ParteA.dbo.Productos (codProducto, descripcion, stock,puntoReposicion,precio) VALUES (2, 'PC', 20, 20,2000.00)
-insert into PrimeraParte_ParteA.dbo.Productos (codProducto, descripcion, stock,puntoReposicion,precio) VALUES (3, 'Parlantes', 30, 50,180.00)
 go
 
 create table PrimeraParte_ParteA.dbo.ItemsPedido (
@@ -129,10 +87,6 @@ create table PrimeraParte_ParteA.dbo.ItemsPedido (
 	constraint fk_ItemsPedido_Productos foreign key (codProducto) references PrimeraParte_ParteA.dbo.Productos (codProducto),
 	constraint fk_ItemsPedido_Pedido foreign key (codPedido) references PrimeraParte_ParteA.dbo.Pedidos (nroPedido)
 )
-
-insert into PrimeraParte_ParteA.dbo.ItemsPedido (codProducto, codPedido, cantidad) VALUES (1, 2, 5)
-insert into PrimeraParte_ParteA.dbo.ItemsPedido (codProducto, codPedido, cantidad) VALUES (2, 3, 10)
-insert into PrimeraParte_ParteA.dbo.ItemsPedido (codProducto, codPedido, cantidad) VALUES (3, 1, 15)
 go
 
 create table PrimeraParte_ParteA.dbo.ItemsFactura(
@@ -144,10 +98,6 @@ create table PrimeraParte_ParteA.dbo.ItemsFactura(
 	constraint fk_ItemsFactura_Productos foreign key (codProducto) references PrimeraParte_ParteA.dbo.Productos (codProducto),
 	constraint fk_ItemsFactura_Factura foreign key (nroFactura) references PrimeraParte_ParteA.dbo.Facturas (nroFactura)
 )
-
-insert into PrimeraParte_ParteA.dbo.ItemsFactura (codProducto, nroFactura, cantidad, precio, descuento) VALUES (1, 1, 1,275,0)
-insert into PrimeraParte_ParteA.dbo.ItemsFactura (codProducto, nroFactura, cantidad, precio, descuento) VALUES (2, 2, 1,2200,0)
-insert into PrimeraParte_ParteA.dbo.ItemsFactura (codProducto, nroFactura, cantidad, precio, descuento) VALUES (3, 3, 1,198,0)
 go
 
 create table PrimeraParte_ParteA.dbo.Composiciones(
@@ -158,12 +108,75 @@ create table PrimeraParte_ParteA.dbo.Composiciones(
 	constraint pk_ProductoComponente primary key (codProductoComponente),
 	constraint fk_Producto foreign key (codProducto) references PrimeraParte_ParteA.dbo.Productos (codProducto)
 )
+go
 
+
+--Insert Artistas
+insert into PrimeraParte_ParteA.dbo.Artista (codArtista, nombre) VALUES (1, 'Dua Lipa')
+insert into PrimeraParte_ParteA.dbo.Artista (codArtista, nombre) VALUES (2, 'Maroon 5')
+insert into PrimeraParte_ParteA.dbo.Artista (codArtista, nombre) VALUES (3, 'YSY A')
+insert into PrimeraParte_ParteA.dbo.Artista (codArtista, nombre) VALUES (4, 'Guns and Roses')
+insert into PrimeraParte_ParteA.dbo.Artista (codArtista, nombre) VALUES (5, 'Ludwig van Beethoven')
+go
+--Insert Generos
+insert into PrimeraParte_ParteA.dbo.Generos (codGenero, descripcion) VALUES (1, 'POP')
+insert into PrimeraParte_ParteA.dbo.Generos (codGenero, descripcion) VALUES (2, 'TRAP')
+insert into PrimeraParte_ParteA.dbo.Generos (codGenero, descripcion) VALUES (3, 'ROCK')
+insert into PrimeraParte_ParteA.dbo.Generos (codGenero, descripcion) VALUES (4, 'CLASICO')
+go
+--Insert Albumes
+insert into PrimeraParte_ParteA.dbo.Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (1,'Future Nostalgia', '27-03-2020', 1, 1)
+insert into PrimeraParte_ParteA.dbo.Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (2,'JODRI (Deluxe)', '11-06-2021', 2, 1)
+insert into PrimeraParte_ParteA.dbo.Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (3,'TRAP DE VERDAD', '11-11-2021', 3, 2)
+insert into PrimeraParte_ParteA.dbo.Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (4,'Use Your Illusion I', '17-09-1991', 4, 3)
+insert into PrimeraParte_ParteA.dbo.Albumes (codAlbum, titulo, fecha, codArtista, codGenero) VALUES (5,'Sinfonias', '01-01-1785', 5, 3)
+go
+--Insert Temas
+insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (1, 'Future Nostalgia', 3.04, 1)
+insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (2, 'Dont Start Now', 3.03, 1)
+insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (3, 'Levitating', 3.23, 1)
+
+insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (4, 'Beautiful Mistakes', 3.47, 2)
+insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (5, 'Lost', 2.53, 2)
+insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (6, 'Echo', 2.59, 2)
+
+insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (7, 'TRAP DE VERDAD', 2.12, 3)
+insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (8, 'ORO Y PLATINO', 2.47, 3)
+insert into PrimeraParte_ParteA.dbo.Temas (codTema, titulo,duracion,codAlbum) VALUES (9, 'INSPIRACION DIVINA', 3.11, 3)
+go
+--Insert Clientes
+insert into PrimeraParte_ParteA.dbo.Cliente (codCliente, nombre, saldo) VALUES (1, 'Jose Luis', 87.40)
+insert into PrimeraParte_ParteA.dbo.Cliente (codCliente, nombre, saldo) VALUES (2, 'Constantino', 7.30)
+insert into PrimeraParte_ParteA.dbo.Cliente (codCliente, nombre, saldo) VALUES (3, 'Matias', 999.99)
+go
+--Insert Pedidos
+insert into PrimeraParte_ParteA.dbo.Pedidos (nroPedido, fecha, codCliente,fechaEntrega,lugarEntrega) VALUES (1, '28-06-2022', 1, '01-07-2022', 'Bernardo de Irigoyen 924')
+insert into PrimeraParte_ParteA.dbo.Pedidos (nroPedido, fecha, codCliente,fechaEntrega,lugarEntrega) VALUES (2, '30-06-2022', 3, '23-07-2022', 'Lima 755')
+insert into PrimeraParte_ParteA.dbo.Pedidos (nroPedido, fecha, codCliente,fechaEntrega,lugarEntrega) VALUES (3, '02-06-2022', 2, '10-06-2022', 'Av. Independencia 985')
+go
+--Insert Facturas
+insert into PrimeraParte_ParteA.dbo.Facturas (nroFactura, fecha, codCliente,codPedido,bruto,iva,IIBB,final) VALUES (1, '28-06-2022', 1, 1,250.00,10.0,0.0,275.0)
+insert into PrimeraParte_ParteA.dbo.Facturas (nroFactura, fecha, codCliente,codPedido,bruto,iva,IIBB,final) VALUES (2, '30-06-2022', 3, 2,2000.00,10.0,0.0,2200.0)
+insert into PrimeraParte_ParteA.dbo.Facturas (nroFactura, fecha, codCliente,codPedido,bruto,iva,IIBB,final) VALUES (3, '02-06-2022', 2, 1,180.00,10.0,0.0,198.0)
+go
+--Insert Productos
+insert into PrimeraParte_ParteA.dbo.Productos (codProducto, descripcion, stock,puntoReposicion,precio) VALUES (1, 'Walkman', 15, 5,250.00)
+insert into PrimeraParte_ParteA.dbo.Productos (codProducto, descripcion, stock,puntoReposicion,precio) VALUES (2, 'PC', 20, 20,2000.00)
+insert into PrimeraParte_ParteA.dbo.Productos (codProducto, descripcion, stock,puntoReposicion,precio) VALUES (3, 'Parlantes', 30, 50,180.00)
+go
+--Insert ItemsPedido
+insert into PrimeraParte_ParteA.dbo.ItemsPedido (codProducto, codPedido, cantidad) VALUES (1, 2, 5)
+insert into PrimeraParte_ParteA.dbo.ItemsPedido (codProducto, codPedido, cantidad) VALUES (2, 3, 10)
+insert into PrimeraParte_ParteA.dbo.ItemsPedido (codProducto, codPedido, cantidad) VALUES (3, 1, 15)
+go
+--Insert ItemsFactura
+insert into PrimeraParte_ParteA.dbo.ItemsFactura (codProducto, nroFactura, cantidad, precio, descuento) VALUES (1, 1, 1,275,0)
+insert into PrimeraParte_ParteA.dbo.ItemsFactura (codProducto, nroFactura, cantidad, precio, descuento) VALUES (2, 2, 1,2200,0)
+insert into PrimeraParte_ParteA.dbo.ItemsFactura (codProducto, nroFactura, cantidad, precio, descuento) VALUES (3, 3, 1,198,0)
+go
+--Insert Composiciones
 insert into PrimeraParte_ParteA.dbo.Composiciones VALUES (1,1,100)
 insert into PrimeraParte_ParteA.dbo.Composiciones VALUES (2,2,25)
 insert into PrimeraParte_ParteA.dbo.Composiciones VALUES (3,3,400)
 insert into PrimeraParte_ParteA.dbo.Composiciones VALUES (2,4,50)
 go
-
-
-
