4ff6a2be55efaf466882d4d2684941eada6e690b
Jose Luis Koller <joseluiskoller98@gmail.com>
2022-07-02 15:25:17 -0300

    Para vos tino, que sean solidas esas queries

--- InsertadorDatos.sql
+++ InsertadorDatos.sql
@@ -3,7 +3,10 @@
 INSERT INTO Pacientes 
 	VALUES 
 	(1, 'Constantino', 'Monteavaro', 'M', '28/10/2001'),
-	(2, 'Luciana', 'Ramirez', 'F', '21/05/1992')
+	(2, 'Luciana', 'Ramirez', 'F', '21/05/1992'),
+	(3, 'Paloma', 'Koller', 'F', '01/03/2000'),
+	(4, 'Antonio', 'Perez', 'M', '01/10/1996'),
+	(5, 'Ezequiel', 'Estevez', 'M', '12/02/1994')
 
 INSERT INTO OOSS 
 	VALUES 
@@ -22,25 +25,46 @@ INSERT INTO Planes
 INSERT INTO Afiliados 
 	VALUES 
 	(1, 'OSDE', 1, 1),
-	(2, 'OSDE', 1, 2)
+	(2, 'OSDE', 1, 2),
+	(3, 'GALE', 1, 1),
+	(4, 'OSDE', 2, 2),
+	(5, 'MEDI', 1, 5)
 
 INSERT INTO Estudios 
 	VALUES 
 	(3, 'Tomografia', 1),
 	(1, 'Radiografia', 1),
-	(2, 'Oftalmologia', 1)
+	(2, 'Oftalmologia', 1),
+	(4, 'Laboratorio', 1),
+	(5, 'Ecografia', 0),
+	(6, 'Hisopado', 1)
 
 INSERT INTO Coberturas 
 	VALUES 
-	('OSDE', 2, 2, 90), 
+	--Cobertura Radio
 	('GALE', 1,1,100), 
 	('OSDE', 1, 1, 100), 
-	('OSDE', 1, 2, 80)  
+	--Cobertura Oftalmologia
+	('OSDE', 2, 2, 90), 
+	('OSDE', 1, 2, 80),  
+	--Cobertura Tomografia
+	('MEDI', 1, 3, 80),
+	('GALE', 2, 3, 80),
+	--Cobertura Laboratorio
+	('MEDI', 1, 4, 67),
+	('OSDE', 1, 4, 10),
+	--Cobertura Ecografia
+	('OSDE', 1, 5, 95),
+	('OSDE', 2, 5, 30),
+	('GALE', 1, 5, 45),
+	('MEDI', 1, 5, 20)
 
 INSERT INTO Institutos 
 	VALUES 
 	(1, 'Trinidad Palermo', 1),
-	(2, 'Hospital Fernandez', 1)
+	(2, 'Hospital Fernandez', 1),
+	(3, 'Policlinico', 1),
+	(4, 'ADOS', 0)
 
 INSERT INTO Precios 
 	VALUES 
