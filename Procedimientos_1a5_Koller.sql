426ab3e8bb976e0e49213a933f4fac5c14f0d85c
Jose Luis Koller <joseluiskoller98@gmail.com>
2022-07-02 20:55:26 -0300

    Se avanzo?

--- Procedimientos_1a5_Koller.sql
+++ Procedimientos_1a5_Koller.sql
@@ -2,18 +2,18 @@ CREATE PROCEDURE updatePrecioEstudio
 	(@nombreEstudio VARCHAR(50), @nombreInstituto VARCHAR(50), @precioNuevo float) 
 	AS
 	BEGIN
-		IF @nombrePlan IS NOT NULL
-			SELECT est.estudio, cob.cobertura, pla.nombre 
-				FROM estudios est
-					INNER JOIN coberturas cob ON est.idestudio = cob.idestudio
-					INNER JOIN (SELECT * FROM Planes WHERE nombre = @nombrePlan) pla on cob.nroplan = pla.nroplan and cob.sigla = pla.sigla
-						WHERE 
-							(SELECT sigla FROM ooss WHERE nombre = @nombreOoss) = cob.sigla 			
-		ELSE
-			SELECT est.estudio, cob.cobertura, pla.nombre 
-				FROM estudios est
-					INNER JOIN coberturas cob ON est.idestudio = cob.idestudio
-					INNER JOIN Planes pla on cob.nroplan = pla.nroplan and cob.sigla = pla.sigla
-					WHERE (SELECT sigla FROM ooss WHERE nombre = @nombreOoss) = cob.sigla				
+		if exists (select instituto, estudio from institutos
+					inner join precios on institutos.idinstituto = precios.idinstituto
+					inner join estudios on estudios.idestudio = precios.idestudio
+					where estudio = @nombreEstudio or instituto = @nombreInstituto)
+			if exists (select precio from precios 
+					inner join institutos on institutos.idinstituto = precios.idinstituto
+					inner join estudios on estudios.idestudio = precios.idestudio)
+			update precios set precio = @precioNuevo 
+			
+		else
+			
 	END
 GO
+
+--If EXISTS (select precio from precios where precio is not null)
