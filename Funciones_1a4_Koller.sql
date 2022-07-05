aeba0b8ae3135180818d5350c98ba8f6b7e989d7
Jose Luis Koller <joseluiskoller98@gmail.com>
2022-07-05 11:57:39 -0300

    Funciones listas menos 1 xq paja

--- Funciones_1a4_Koller.sql
+++ Funciones_1a4_Koller.sql
@@ -9,16 +9,55 @@ BEGIN
 END
 go
 
+--Ejer 2
 CREATE FUNCTION preciosEstudio
 	(@nombreEstudio varchar(50))
-RETURNS @precios TABLE(mayorPrecio float, menorPrecio float, precioPromedio float)
+RETURNS @preciosEstudio TABLE(mayorPrecio float, menorPrecio float, precioPromedio float)
 AS
 BEGIN
 		declare @mayor float
 		declare @menor float
 		declare @prom float
-		select precio from precios 
+		select @prom = avg(precio) from precios 
 			inner join estudios on precios.idestudio = estudios.idestudio
+			where estudio = @nombreEstudio 
+		select @mayor = max(precio)from precios 
+			inner join estudios on precios.idestudio = estudios.idestudio
+			where estudio = @nombreEstudio
+		select @menor = min(precio)from precios 
+			inner join estudios on precios.idestudio = estudios.idestudio
+			where estudio = @nombreEstudio
+		insert into @preciosEstudio values (@mayor, @menor, @prom)
 		RETURN
 END
+GO
+
+--Ejer 3
+create function tablaInfoAlfa
+()
+returns @tablaFinal table(ooss varchar(50), especialidades varchar(50), institutos varchar(50), estudios varchar(50))
+as
+	BEGIN
+		insert into @tablaFinal (ooss) select nombre from ooss order by nombre ASC
+		insert into @tablaFinal (especialidades) select especialidad from especialidades order by especialidad ASC
+		insert into @tablaFinal (institutos) select instituto from institutos order by instituto ASC
+		insert into @tablaFinal (estudios) select estudio from estudios order by estudio ASC
+		RETURN
+	END
+GO
+
+--Ejer 4
+
+--Ejer 5
+create function estudiosNoRealizados
+	(@dias int)
+returns @tablaFinal table (idestudio int, nombreEstudio varchar(50))
+AS
+	BEGIN
+		insert into @tablaFinal (idestudio, nombreEstudio)
+			select historias.idestudio, estudio from historias 
+				inner join estudios on historias.idestudio = estudios.idestudio
+				where fecha between DATEADD(d, -@dias, GETDATE()) and getdate()
+		RETURN
+	END
 GO
