51f0be8e656640eaed2289a5e50e21c321ab7f22
Jose Luis Koller <joseluiskoller98@gmail.com>
2022-07-04 21:59:57 -0300

    Toy cansado

--- Funciones_1a4_Koller.sql
+++ Funciones_1a4_Koller.sql
@@ -8,3 +8,17 @@ BEGIN
 		DATEDIFF(YYYY, @fechaNacimiento, getDate())
 END
 go
+
+CREATE FUNCTION preciosEstudio
+	(@nombreEstudio varchar(50))
+RETURNS @precios TABLE(mayorPrecio float, menorPrecio float, precioPromedio float)
+AS
+BEGIN
+		declare @mayor float
+		declare @menor float
+		declare @prom float
+		select precio from precios 
+			inner join estudios on precios.idestudio = estudios.idestudio
+		RETURN
+END
+GO
