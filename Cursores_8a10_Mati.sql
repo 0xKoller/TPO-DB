5ee4497079393ca81de0c61229810b56721b0fcc
VeintiTreinta <matisantoro10@gmail.com>
2022-07-07 18:02:21 -0300

    Cursor 8 completado. Anda a saber si está bien o no xd

--- Cursores_8a10_Mati.sql
+++ Cursores_8a10_Mati.sql
@@ -1,6 +1,6 @@
 SET DATEFORMAT DMY
 GO
---EJER 8 NO FUNCIONA EL PIVOT PARA HACER LAS TABLAS CRUZADAS
+--EJER 8
 CREATE PROC sp_definirCursorImporteMensual
 	(@mesesAnteriores int)
 AS 
@@ -9,24 +9,25 @@ BEGIN
 		SELECT P.precio,H.fecha,P.idinstituto FROM precios P INNER JOIN historias H ON P.idinstituto=H.idinstituto
 		WHERE H.fecha BETWEEN (DATEADD(month,-@mesesAnteriores,GETDATE())) AND GETDATE()
 		GROUP BY P.idinstituto,P.precio,h.fecha
+	
 	DECLARE @precio int,@fecha date, @idInstituto int
+	DECLARE @resultado TABLE (precio int, fecha date, idInsti int)
+	
 	OPEN importeMensual
 
-	FETCH NEXT FROM importeMensual INTO @precio,@fecha,@idInstituto
+	FETCH importeMensual INTO @precio,@fecha,@idInstituto
 	WHILE @@FETCH_STATUS = 0
 		BEGIN
-			
-
-
-			FETCH NEXT FROM importeMensual INTO @precio,@fecha,@idInstituto
+			INSERT @resultado VALUES (@precio,@fecha,@idInstituto)
+			FETCH importeMensual INTO @precio,@fecha,@idInstituto
 		END
-
 	CLOSE importeMensual
 	DEALLOCATE importeMensual
-
 END
 GO
 
+EXEC sp_definirCursorImporteMensual 3
+GO
 
 --EJER 9
 DECLARE actualizarObservaciones CURSOR FOR
