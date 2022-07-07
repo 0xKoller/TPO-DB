f396ecc4151bcaea72fa7df9f128b8c9692a6597
Jose Luis Koller <joseluiskoller98@gmail.com>
2022-07-07 14:59:58 -0300

    Cursores

--- Cursores_4a7_Koller.sql
+++ Cursores_4a7_Koller.sql
@@ -1,35 +1,3 @@
-
-DECLARE estudiosPorInstitutos CURSOR FOR 
-	SELECT pac.dni, nombre, apellido, instituto, cantEstudios
-		FROM	
-			(SELECT dni,idInstituto, COUNT(idEstudio) cantEstudios
-				FROM Historias 
-				GROUP BY dni,idInstituto) tab
-		INNER JOIN pacientes pac on tab.dni = pac.dni
-		INNER JOIN institutos ins on tab.idInstituto = ins.idInstituto
-		ORDER BY pac.dni
-
-DECLARE @dni int, @nombre varchar(100), @apellido varchar(100), @instituto varchar(100), @cantEstudios varchar(100)
-DECLARE @dniAnt int, @cantEstudiosPac int
-SET @dniAnt = 0
-OPEN estudiosPorInstitutos
-FETCH estudiosPorInstitutos into @dni, @nombre, @apellido, @instituto, @cantEstudios
-WHILE @@FETCH_STATUS = 0
-	BEGIN
-		IF @dni <> @dniAnt
-			BEGIN
-				PRINT('	'+CONVERT(VARCHAR(10), @cantEstudiosPac))
-				PRINT(CONVERT(VARCHAR(10), @dni)+' '+@nombre+' '+@apellido) 
-				SET @cantEstudiosPac = 0
-			END
-		print('	'+@instituto + ' '+ CONVERT(VARCHAR(10), @cantEstudios))
-		SET @cantEstudiosPac = @cantEstudiosPac + @cantEstudios
-		SET @dniAnt = @dni
-		FETCH estudiosPorInstitutos into @dni, @nombre, @apellido, @instituto, @cantEstudios
-	END
-CLOSE estudiosPorInstitutos
-DEALLOCATE estudiosPorInstitutos
-
 -- 4)
 DECLARE estudiosPorMedico CURSOR FOR
 	SELECT med.nombre, med.matricula, estudio, fecha, paci.nombre
@@ -38,8 +6,7 @@ DECLARE estudiosPorMedico CURSOR FOR
 			INNER JOIN pacientes paci ON hist.dni = paci.dni
 			INNER JOIN estudios estu ON hist.idestudio = estu.idestudio
 DECLARE @medNombre VARCHAR(50), @medMatri INT, @estudio VARCHAR(50), @fecha DATE, @paciNombre VARCHAR(50)
-DECLARE @matriAnt INT, @cantEstudiosPorMed INT
---Canttidad del estudio ??
+DECLARE @matriAnt INT, @cantEstudiosPorMed INT, @cantMismoEstudio INT, @estuAnt VARCHAR(50)
 SET @matriAnt = 0
 OPEN estudiosPorMedico
 FETCH estudiosPorMedico INTO @medNombre, @medMatri, @estudio, @fecha, @paciNombre
@@ -47,14 +14,103 @@ WHILE @@FETCH_STATUS = 0
 	BEGIN
 		IF @medMatri <> @matriAnt
 			BEGIN
-				PRINT(' '+CONVERT(VARCHAR(10), @cantEstudiosPorMed)) -- Esto no funciona nose porque
+				PRINT('	'+CONVERT(VARCHAR(10), @cantEstudiosPorMed))
+				SET @cantEstudiosPorMed = 0
+				SET @estuAnt = @estudio
 				PRINT(CONVERT(VARCHAR(10), @medMatri)+' '+@medNombre)
+				PRINT('	'+@estudio)
 			END
-		PRINT('	'+@estudio)
+		IF @estudio <> @estuAnt
+			BEGIN
+				PRINT('		'+CONVERT(VARCHAR(20), @cantMismoEstudio))
+				SET @cantMismoEstudio = 0
+			END
 		PRINT('		'+CONVERT(VARCHAR(20),@fecha)+' '+@paciNombre)
 		SET @cantEstudiosPorMed = @cantEstudiosPorMed + 1
+		SET @cantMismoEstudio = @cantMismoEstudio + 1
 		SET @matriAnt = @medMatri
 		FETCH estudiosPorMedico INTO @medNombre, @medMatri, @estudio, @fecha, @paciNombre
 	END
 CLOSE estudiosPorMedico
 DEALLOCATE estudiosPorMedico
+GO
+
+-- 5)
+CREATE PROCEDURE resumenMensualCursor
+	(@nombreOOSS VARCHAR(20), @fechaLiquidar DATE)
+	AS
+	BEGIN
+		DECLARE resumenMensual CURSOR FOR
+			SELECT nombre,  instituto, estudio, precio, fecha
+			FROM ooss os INNER JOIN historias his ON os.sigla = his.sigla 
+			INNER JOIN institutos inst ON inst.idinstituto = his.idinstituto
+			INNER JOIN estudios estu ON estu.idestudio = his.idestudio
+			INNER JOIN precios prec ON prec.idestudio = his.idestudio
+		DECLARE @OOSS VARCHAR(20),@instituto VARCHAR(20) , @estudio VARCHAR(20), @precio FLOAT, @fecha DATE
+		DECLARE  @instAnt VARCHAR(20), @totalInst FLOAT, @totalOOSS FLOAT, @flag INT
+		SET @instAnt = ''
+		SET @totalOOSS = 0
+		SET @flag = 0
+		OPEN resumenMensual
+		FETCH resumenMensual INTO @OOSS, @instituto, @estudio, @precio
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+			IF @OOSS = @nombreOOSS
+				BEGIN
+					IF MONTH(@fechaLiquidar) = MONTH(@fecha) AND YEAR(@fechaLiquidar) = YEAR(@fecha)
+						BEGIN
+							BEGIN
+								IF @flag = 0
+									BEGIN
+										PRINT(@OOSS)
+									END
+								IF @instituto <> @instAnt
+									BEGIN
+										PRINT(' '+@totalInst)
+										PRINT('	'+@instituto)
+										SET @totalInst = 0
+									END
+								ELSE
+									BEGIN
+										PRINT('		'+@estudio+'	'+@precio)
+										SET @totalInst = @totalInst + @precio
+										SET @totalOOSS = @totalOOSS + @precio
+									END
+							END
+						END
+				END
+			FETCH resumenMensual INTO @OOSS, @instituto, @estudio, @precio
+		END
+		PRINT(@totalOOSS)
+	END
+CLOSE resumenMensualCursor
+DEALLOCATE resumenMensualCursor
+GO
+
+-- 6)
+CREATE PROCEDURE refeCruzadaOOSSEstuCursor
+	(@nombreOOSS VARCHAR(20))
+	AS
+	BEGIN
+		DECLARE refCruzadaOOSSEstu CURSOR FOR
+			SELECT os.nombre, pla.nroplan, estudio, count(pla.nroplan) as cantXPlan FROM historias his
+				INNER JOIN afiliados af ON af.sigla = his.sigla
+				INNER JOIN estudios est ON est.idestudio = his.idestudio
+				INNER JOIN planes pla ON pla.nroplan = af.nroplan
+				INNER JOIN ooss os ON os.sigla = his.sigla
+				GROUP BY his.sigla,pla.nroplan, estudio, os.nombre
+		DECLARE @OOSS VARCHAR(20), @nroPlan INT, @nombreEstudio VARCHAR(20), @cantXPlan INT
+		DECLARE @tablaResultante TABLE (nroplan INT, estudio VARCHAR(20), cant INT)
+		OPEN refCruzadaOOSSEstu
+		FETCH refCruzadaOOSSEstu INTO @OOSS, @nroPlan, @nombreEstudio, @cantXPlan
+		WHILE @@FETCH_STATUS = 0
+			BEGIN
+				IF @OOSS = @nombreOOSS
+					INSERT @tablaResultante VALUES(@nroPlan, @nombreEstudio, @cantXPlan)
+				FETCH refCruzadaOOSSEstu INTO @OOSS, @nroPlan, @nombreEstudio, @cantXPlan
+			END	
+		CLOSE refCruzadaOOSSEstu
+		DEALLOCATE refCruzadaOOSSEstu
+	END
+GO
+
