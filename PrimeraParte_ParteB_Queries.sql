128cbe33ff10e33b4780134c94ca6141a95d788e
VeintiTreinta <matisantoro10@gmail.com>
2022-07-07 20:57:28 -0300

    Querys Parte B completadas

--- PrimeraParte_ParteB_Queries.sql
+++ PrimeraParte_ParteB_Queries.sql
@@ -1,14 +1,22 @@
 --Ejer 14
-select nombre, alias, privilegio, idCategoria from usuarios inner join privilegios on usuarios.idUsuario = privilegios.idUsuario
+SELECT nombre, alias, privilegio, idCategoria FROM usuarios INNER JOIN privilegios ON usuarios.idUsuario = privilegios.idUsuario
 
---Ejer 15 aca hay que agregar un max, x ahora pajita
-select alias, noticias.idNoticia from noticias 
-	inner join (select idNoticia,SUM(votosUno + votosDos + votosTres) as TotalVotaciones from votaciones group by idNoticia) as TablaResultante on noticias.idNoticia = TablaResultante.idNoticia 
-	inner join usuarios on usuarios.idUsuario = noticias.autor
+--Ejer 15
+SELECT H.alias,T.[Votaciones Totales] FROM (
+SELECT MAX(F.TotalVotaciones) 'Votaciones Totales'
+FROM
+(SELECT U.alias, SUM(votosUno+votosDos+votosTres) TotalVotaciones FROM votaciones V INNER JOIN noticias N ON V.idNoticia=N.idNoticia
+INNER JOIN usuarios U ON N.autor=U.idUsuario GROUP BY U.alias) F) T
+INNER JOIN (SELECT U.alias, SUM(votosUno+votosDos+votosTres) TotalVotaciones FROM votaciones V INNER JOIN noticias N ON V.idNoticia=N.idNoticia
+INNER JOIN usuarios U ON N.autor=U.idUsuario GROUP BY U.alias) H ON H.TotalVotaciones=T.[Votaciones Totales]
 
 --Ejer 16
+SELECT N.titulo Noticia,SUM(votosUno + votosDos + votosTres) AS 'Votaciones Totales' FROM votaciones V 
+INNER JOIN noticias N ON V.idNoticia=N.idNoticia group by N.titulo
 
 --Ejer 17
-
---Ejer 18
+SELECT alias, N.titulo Noticia FROM usuarios U INNER JOIN noticias N ON U.idUsuario=N.autor
+INNER JOIN categorias C ON N.idCategoria=C.idCategoria
+WHERE C.nombre='Policiales'
+AND N.idNoticia = ANY(SELECT idNoticia FROM comentarios)
 
--- PrimeraParte_ParteB_Tablas.sql
+++ PrimeraParte_ParteB_Tablas.sql
@@ -74,35 +74,35 @@ create table PrimeraParte_ParteB.dbo.votaciones(
 go
 
 --Insert Usuarios
-insert into usuarios (idUsuario, nombre, alias, contra, activo, correo) values (0, 'Jose Luis', 'SR Developer', '123$%^789', 1, 'seniordeveloper@koller.com')
-insert into usuarios (idUsuario, nombre, alias, contra, activo, correo) values (1, 'Constantino', 'Web Scrapper Officer', 'pythonrocks', 1, 'pythonesco@gmail.com')
-insert into usuarios (idUsuario, nombre, alias, contra, activo, correo) values (2,'Matias', 'PHP Rocks', 'phpisdead', 1, 'myownemail@matute.com')
-insert into usuarios (idUsuario, nombre, alias, contra, activo, correo) values (3, 'Latour', 'Oxford dude', 'guater', 0, 'cambridge@gmail.com')
+insert into PrimeraParte_ParteB.dbo.usuarios (idUsuario, nombre, alias, contra, activo, correo) values (0, 'Jose Luis', 'SR Developer', '123$%^789', 1, 'seniordeveloper@koller.com')
+insert into PrimeraParte_ParteB.dbo.usuarios (idUsuario, nombre, alias, contra, activo, correo) values (1, 'Constantino', 'Web Scrapper Officer', 'pythonrocks', 1, 'pythonesco@gmail.com')
+insert into PrimeraParte_ParteB.dbo.usuarios (idUsuario, nombre, alias, contra, activo, correo) values (2,'Matias', 'PHP Rocks', 'phpisdead', 1, 'myownemail@matute.com')
+insert into PrimeraParte_ParteB.dbo.usuarios (idUsuario, nombre, alias, contra, activo, correo) values (3, 'Latour', 'Oxford dude', 'guater', 0, 'cambridge@gmail.com')
 go
 
 --Insert Categorias
-insert into categorias(idCategoria, nombre, descripcion) values (0, 'Policiales', 'Noticias de incidentes, accidentes, transito, manifestaciones')
-insert into categorias(idCategoria, nombre, descripcion) values (1, 'Politica', 'Destapando verdades de la politica Argentina')
-insert into categorias(idCategoria, nombre, descripcion) values (2, 'Deportes', 'Toda la informacion sobre copas nacionales y mundiales')
+insert into PrimeraParte_ParteB.dbo.categorias(idCategoria, nombre, descripcion) values (0, 'Policiales', 'Noticias de incidentes, accidentes, transito, manifestaciones')
+insert into PrimeraParte_ParteB.dbo.categorias(idCategoria, nombre, descripcion) values (1, 'Politica', 'Destapando verdades de la politica Argentina')
+insert into PrimeraParte_ParteB.dbo.categorias(idCategoria, nombre, descripcion) values (2, 'Deportes', 'Toda la informacion sobre copas nacionales y mundiales')
 go
 
 --Insert Noticias
-insert into noticias(idNoticia, titulo, idCategoria, autor, fecha, ubicacion) values (0, 'Se robaron hasta el papel higenico', 0, 1, '01/07/2022', 'Chacarita')
-insert into noticias(idNoticia, titulo, idCategoria, autor, fecha, ubicacion) values (1, 'No dejaron ni los cordones', 0, 2, '11/03/2021', 'Neuquen')
+insert into PrimeraParte_ParteB.dbo.noticias(idNoticia, titulo, idCategoria, autor, fecha, ubicacion) values (0, 'Se robaron hasta el papel higenico', 0, 1, '01/07/2022', 'Chacarita')
+insert into PrimeraParte_ParteB.dbo.noticias(idNoticia, titulo, idCategoria, autor, fecha, ubicacion) values (1, 'No dejaron ni los cordones', 0, 2, '11/03/2021', 'Neuquen')
 go
 
 --Insert Comentarios
-insert into comentarios(idComentario,idNoticia, texto, idUsuario, fecha) values (1, 1,'NEUQUEN ES COMPROMISO', 0, '11/03/2021')
+insert into PrimeraParte_ParteB.dbo.comentarios(idComentario,idNoticia, texto, idUsuario, fecha) values (1, 1,'NEUQUEN ES COMPROMISO', 0, '11/03/2021')
 go
 
 --Insert Privilegios
-insert into privilegios(idUsuario, privilegio, idCategoria) values (0, 'Responsable', 0)
-insert into privilegios(idUsuario, privilegio, idCategoria) values (1, 'Crear Noticia', 0)
-insert into privilegios(idUsuario, privilegio, idCategoria) values (2, 'Crear Noticia', 1)
-insert into privilegios(idUsuario, privilegio, idCategoria) values (3, 'Responsable', 1)
+insert into PrimeraParte_ParteB.dbo.privilegios(idUsuario, privilegio, idCategoria) values (0, 'Responsable', 0)
+insert into PrimeraParte_ParteB.dbo.privilegios(idUsuario, privilegio, idCategoria) values (1, 'Crear Noticia', 0)
+insert into PrimeraParte_ParteB.dbo.privilegios(idUsuario, privilegio, idCategoria) values (2, 'Crear Noticia', 1)
+insert into PrimeraParte_ParteB.dbo.privilegios(idUsuario, privilegio, idCategoria) values (3, 'Responsable', 1)
 go
 
 --Insert Votaciones
-insert into votaciones(idVotacion, idNoticia, fechaInicio, fechaFin, pregunta, respUno, respDos, respTres, votosUno, votosDos, votosTres) values (0, 0, '01/07/2022', '08/07/2022', 'Era necesario?', 'Totalmente', 'No lo se', 'Indignante', 50, 4, 84)
-insert into votaciones(idVotacion, idNoticia, fechaInicio, fechaFin, pregunta, respUno, respDos, respTres, votosUno, votosDos, votosTres) values (1, 1, '11/03/2021', '18/03/2021', 'Era necesario?', 'Totalmente', 'No lo se', 'Indignante', 123, 45, 34)
+insert into PrimeraParte_ParteB.dbo.votaciones(idVotacion, idNoticia, fechaInicio, fechaFin, pregunta, respUno, respDos, respTres, votosUno, votosDos, votosTres) values (0, 0, '01/07/2022', '08/07/2022', 'Era necesario?', 'Totalmente', 'No lo se', 'Indignante', 50, 4, 84)
+insert into PrimeraParte_ParteB.dbo.votaciones(idVotacion, idNoticia, fechaInicio, fechaFin, pregunta, respUno, respDos, respTres, votosUno, votosDos, votosTres) values (1, 1, '11/03/2021', '18/03/2021', 'Era necesario?', 'Totalmente', 'No lo se', 'Indignante', 123, 45, 34)
 go
